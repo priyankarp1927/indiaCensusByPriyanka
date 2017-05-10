afb0cc58756ecec0781af93bb9526eaa4d64284a
Admin <priyankaravichandren@gmail.com>
2017-05-10 09:35:28 +0530

    modified indiaCencesByPriyanka

--- js/indiaCensusPriyanka.js
+++ js/indiaCensusPriyanka.js
@@ -1,3 +1,5 @@
+let log4js = require('log4js');
+let logger = log4js.getLogger();
 module.exports = function convert(startYear)
 {
   if(isNaN(startYear))
@@ -14,27 +16,36 @@ let a = [];
 let ageGroup = 0;
 let literatePerson = 0;
 let cleanedLine = [];
-                        let rl = readline.createInterface({
-                        input: fs.createReadStream('../inputdata/final.csv')
-                                                  });
+
+let rl = readline.createInterface({
+  /* CSV file */
+input: fs.createReadStream('../inputdata/final.csv')
+                                         });
 rl.on('line', (line) => {
         cleanedLine = line.split(',');
         if(i === 0)
 				{
+            /* To clean junk files */
             cleanedLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
             data = cleanedLine;
             ageGroup = data.indexOf('Age-group');
             literatePerson = data.indexOf('Literate - Persons');
             i = i + 1;
 				}
- data = line.split(',');
 
+    data = line.split(',');
     a.push({agegroup: data[ageGroup], literateperson: data[literatePerson]});
+    logger.debug(a);
 });
+
+
 rl.on('close', (line) => {
-  cleanedLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-  fs.writeFile('../outputdata/ic.json', JSON.stringify(a));
+        cleanedLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+       /* To write a output */
+        fs.writeFile('../outputdata/outputIndiaCensusPriyanka.json', JSON.stringify(a));
+        logger.debug('File Closed');
 });
+
 return 'JSON written successfully';
 }
 }
